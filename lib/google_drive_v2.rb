8016efcaa7015b3615bff57ec5ae941f75e6b168
Denis Rozenkin <rozenkin@gmail.com>
2016-03-31 18:59:01 +0300

    fixing strings

--- lib/google_drive_v2.rb
+++ lib/google_drive_v2.rb
@@ -3,7 +3,7 @@ drive = Google::Apis::DriveV2::DriveService.new
 drive.authorization = db.authorization
 table_file = drive.list_files.items.find { |file| file.id == db.get_table_by(name: 'macroregions').table_id }
 perm = Google::Apis::DriveV2::Permission.new
-perm.value = 'rozenkin@gmail.com'
+perm.value = 'test@email.com'
 perm.role = 'owner'
 perm.type = 'user'
 drive.insert_permission(table_file.id, perm)
